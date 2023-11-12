8f8e83ec77d0efae80189f3ade8d39d6b1d6bbc1
shea0811 <118522130+shea0811@users.noreply.github.com>
2023-11-12 08:59:09 +0000

    Update script.js

--- script.js
+++ script.js
@@ -24,27 +24,30 @@ function displayProductInfo(xml, productCode) {
     var products = xml.getElementsByTagName("product");
     var productInfoSection = document.getElementById('productInfo');
     let htmlContent = '';
-    productInfoSection.innerHTML = ''; // Clear previous results
+    productInfoSection.innerHTML = ''; 
 
     for (var i = 0; i < products.length; i++) {
-        if (products[i].getElementsByTagName("code")[0].childNodes[0].nodeValue === productCode) {
-            var name = products[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
-            var quantity = products[i].getElementsByTagName("quantity")[0].childNodes[0].nodeValue;
-            var price = products[i].getElementsByTagName("price")[0].childNodes[0].nodeValue;
-            
-            // Use the extracted data to create HTML content
-            htmlContent = `<div class="product">
-                               <h2>${name}</h2>
-                               <p>Quantity: ${quantity}</p>
-                               <p>Price: ${price}</p>
-                           </div>`;
-            
-            // Break the loop after finding the matching product
-            break;
+        let productCodeAttribute = products[i].getAttribute("code");
+        if (productCodeAttribute === productCode) {
+            let nameElement = products[i].getElementsByTagName("name")[0];
+            let quantityElement = products[i].getElementsByTagName("quantity")[0];
+            let priceElement = products[i].getElementsByTagName("price")[0];
+
+            if (nameElement && quantityElement && priceElement) {
+                var name = nameElement.childNodes[0].nodeValue;
+                var quantity = quantityElement.childNodes[0].nodeValue;
+                var price = priceElement.childNodes[0].nodeValue;
+
+                htmlContent = `<div class="product">
+                                   <h2>${name}</h2>
+                                   <p>Quantity: ${quantity}</p>
+                                   <p>Price: ${price}</p>
+                               </div>`;
+                break;
+            }
         }
     }
 
     productInfoSection.innerHTML = htmlContent;
 }
-}
-}
+
